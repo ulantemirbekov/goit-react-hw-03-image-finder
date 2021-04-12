f8609a52a79db776cb14cb94740cfa11285bbbe7
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2021-04-12 23:09:57 +0300

    Update Api.js

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const BASE_URL = "https://pixabay.com/api";
+const API_KEY = "19541580-1bdd8df51d74162905bdf8d23";
+
 const fetchPhotos = ({ searchQuery = '', currentPage = 1, perPage = 12 }) => {
     return axios
-        .get(`https://pixabay.com/api/?key=19541580-1bdd8df51d74162905bdf8d23&q=${searchQuery}&page=${currentPage}&image_type=photo&orientation=horizontal&per_page=${perPage}`)
+        .get(`${BASE_URL}/?key=${API_KEY}&q=${searchQuery}&page=${currentPage}&image_type=photo&orientation=horizontal&per_page=${perPage}`)
         .then(response => response.data.hits)
 };
 
